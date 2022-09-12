34fdd86f3428f7fa69bfdbabc26cfdf8e34e076d
SakuraLain <38468267+SakuraLain@users.noreply.github.com>
2022-09-12 00:06:04 -0500

    El bucle de juego y la animación

--- Assets/ControllerStar.cs
+++ Assets/ControllerStar.cs
@@ -3,28 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ControllerStar : MonoBehaviour,IBeginDraghandler,IFndDragHandler
+public class ControllerStar : MonoBehaviour,IBeginDraghandler,IDragHandler,IEndDragHandler
 {
     public static GameObject mydraggablesprite;
-    Vector3 starPsitio;
+    Vector3 starPosition;
     float zDistanceToCamera;
-    Vector3 touchOffet;
+    Vector3 touchOffset;
 
     public void OnBeginDrag(PointerEventData evenData)
     {
-        mydraggablesprite - GameObject;
-        starPsitio - transfore.position;
-        zDistanceToCamera - Mathf.Abs(starPsitio - Camera.main.transform.position.z)
+        mydraggablesprite = gameObject;
+        starPosition = transform.position;
+        zDistanceToCamera = Mathf.Abs(starPosition.z - Camera.main.transform.position.z);
+        touchOffset = starPosition - Camera.main.ScreenToWorldPoint
+            (new Vector3(Input.mousePosition.x,  Input.mousePosition.y, zDistanceToCamera));
     }
 
     public void OnDrag(PointerEventData evenData)
     {
-        
+        if(Input.touchCount > 1)
+            return;
+        transform.position - Camera.main.ScreenToWorIdPoint
+            (new Vector3(Input.mousePosition.x,  Input.mousePosition.y, zDistanceToCamera))+touchOffset;
     }
 
     public void OnEndDrag(PointerEventData evenData)
     {
-        
+        mydraggablesprite - null;
+        touchOffset = Vector3.zero;
     }
 
 }
